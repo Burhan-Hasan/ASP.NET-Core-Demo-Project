644fc516bdd57607b57d852f03a0e1c752fa596b
Burhan <burhan.hasan.dev@gmail.com>
2017-05-05 19:12:39 +0400

    Code in Startup.cs file was commented

--- Demo/Startup.cs
+++ Demo/Startup.cs
@@ -12,6 +12,12 @@ namespace Demo
 {
     public class Startup
     {
+        /*
+         Класс Startup является входной точкой в приложение ASP.NET Core. 
+         Этот класс производит конфигурацию приложения, 
+         настраивает сервисы, которые приложение будет использовать, 
+         устанавливает компоненты для обработки запроса или middleware.
+        */
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -34,27 +40,47 @@ namespace Demo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            //IHostingEnvironment: позволяет взаимодействовать со средой, в которой запускается приложение
+
+            //ILoggerFactory: предоставляет механизм логгирования в приложении
+            /*
+             Метод Configure устанавливает, как приложение будет обрабатывать запрос. 
+             Для установки компонентов, которые обрабатывают запрос, используются методы объекта IApplicationBuilder. 
+             Объект IApplicationBuilder является обязательным параметром для метода Configure.
+             */
+
+            // установка консоли для вывода лога
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            // если приложение в процессе разработки
             if (env.IsDevelopment())
             {
+                // то выводим информацию об ошибке, при наличии ошибки
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
             }
             else
             {
+                // установка обработчика ошибок
                 app.UseExceptionHandler("/Home/Error");
             }
-
+            // установка обработчика статических файлов
             app.UseStaticFiles();
 
+            // Установка компонентов MVC для обработки запроса
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            /*
+             Причем важно, что чтобы использовать некоторые сервисы, их вначале надо зарегистрировать. 
+             Так, мы не смогли бы использовать метод app.UseMvc() в методе Configure, 
+             если бы мы не использовали бы вызов services.AddMvc() в методе ConfigureServices().
+             */
         }
     }
 }
